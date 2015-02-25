f1221414cca484b920623384ba35e69e8e384723
eduardo.skrepnek@gmail.com <eduardo.skrepnek@gmail.com@744d20af-1a78-ebae-58e3-2005926b43d7>
2015-02-25 00:39:50 +0000

    Inicio da lógica de movimentação (girar e mover)

--- trunk/model/Movement.cpp
+++ trunk/model/Movement.cpp
@@ -6,16 +6,89 @@
  */
 
 #include "Movement.h"
+#include <cmath>
 
 Movement::Movement() {
 }
 
 Movement::Movement(Position* start) {
+    Facing* facing = new Facing(90, 0);
+    
     this->start = start;
+    this->facing = facing;
+    this->turnSpeed = 0;
+    
+    this->turning = Movement::NOT_TURNING;
+    this->moving  = Movement::NOT_MOVING;
 }
 
 Movement::Movement(const Movement& orig) {
 }
 
 Movement::~Movement() {
+}
+
+void Movement::moveTo(Position* end){
+    this->end = end;
+    Facing* facingToMove = calcFacingToMove();
+    
+    bool needToTurn = (this->facing->getFacingDirectionXY() != facingToMove->getFacingDirectionXY() ||
+                       this->facing->getFacingDirectionZ() != facingToMove->getFacingDirectionZ());
+    
+    if (needToTurn) {
+        int turningDirection = calculateTurnDirection(facingToMove);
+    }
+    else{
+        // Aqui ele vai andar/correr
+    }
+}
+
+Facing* Movement::calcFacingToMove(){
+    double deltaX = this->end->getX() - this->start->getX();
+    double deltaY = this->end->getY() - this->start->getY();
+    double deltaZ = this->end->getZ() - this->start->getZ();
+    
+    double facingAngleXY = atan2(deltaY, deltaX) * 180 / M_PI;
+    
+    double dist=sqrt(pow(deltaX, 2) + pow(deltaY, 2) + pow(deltaZ, 2));
+    double dist2=sqrt(pow(deltaX, 2) + pow(deltaZ, 2));
+    
+    double facingAngleZ = acos(dist2/dist) * 180 / M_PI;
+    
+    Facing* facing = new Facing((int)round(facingAngleXY), (int)round(facingAngleZ));
+    return facing;
+}
+
+int Movement::calculateTurnDirection(Facing* facingToMove){
+    int angleXYStart = this->facing->getFacingDirectionXY();
+    int angleXYEnd = facingToMove->getFacingDirectionXY();
+    
+}
+
+void move(int typeOfMove){
+    switch(typeOfMove){
+        case Movement::NOT_MOVING:
+            // implementacao
+            break;
+        case Movement::WALKING:
+            // implementacao
+            break;
+        case Movement::RUNNING:
+            // implementacao
+            break;
+    }
+}
+
+void turn(int typeOfTurn){
+    switch(typeOfTurn){
+        case Movement::NOT_TURNING:
+            // implementacao
+            break;
+        case Movement::TURNING_RIGHT:
+            // implementacao
+            break;
+        case Movement::TURNING_LEFT:
+            // implementacao
+            break;
+    }
 }
--- trunk/model/Movement.h
+++ trunk/model/Movement.h
@@ -9,19 +9,35 @@
 #define	MOVEMENT_H
 
 #include "Position.h"
+#include "Facing.h"
 
 class Movement {
 public:
-    /*const double SPEED_STOPPED  = 0.00;
-    const double SPEED_WALKING  = 1.50;
-    const double SPEED_RUNNING  = 5.00;
-    const double MULT_BACKWARDS = 0.50;*/
+    const static int NOT_MOVING = 0;
+    const static int WALKING    = 1;
+    const static int RUNNING    = 2;
+    
+    const static int NOT_TURNING   = 100;
+    const static int TURNING_RIGHT = 101;
+    const static int TURNING_LEFT  = 102;
+    const static int TURNING_UP    = 103;
+    const static int TURNING_DOWN  = 104;
     
     Movement();
     Movement(Position*);
     Movement(const Movement& orig);
     virtual ~Movement();
+    void moveTo(Position*);
+    void move(double, int);
+    
+    void setTurnSpeed(double turnSpeed) {
+        this->turnSpeed = turnSpeed;
+    }
 
+    double getTurnSpeed() const {
+        return turnSpeed;
+    }
+    
     void setSpeed(double speed) {
         this->speed = speed;
     }
@@ -29,13 +45,13 @@ public:
     double getSpeed() const {
         return speed;
     }
-
-    void setDirection(double direction) {
-        this->direction = direction;
+    
+    void setFacing(Facing* facing) {
+        this->facing = facing;
     }
 
-    double getDirection() const {
-        return direction;
+    Facing* getFacing() const {
+        return facing;
     }
 
     void setEnd(Position* end) {
@@ -56,8 +72,18 @@ public:
 private:
     Position* start;
     Position* end;
-    double direction;
+    Facing* facing;
+    
+    int moving;
     double speed;
+    
+    int turning;
+    double turnSpeed;
+    
+    Facing* calcFacingToMove();
+    int calculateTurnDirection(Facing*);
+    void move(int);
+    void turn(int);
 };
 
 #endif	/* MOVEMENT_H */
